da0de4c559c5b11d2810eb8810299d0572b341c8
Cynthia <caron.cynthia@outlook.fr>
2023-04-20 12:37:07 +0200

    fix

--- src/pages/ListEmployee.jsx
+++ src/pages/ListEmployee.jsx
@@ -1,9 +1,30 @@
 import {useSelector} from "react-redux";
+import {useState} from "react";
 import {Link} from "react-router-dom";
 
 function ListEmployee() {
   const employees = useSelector((state) => state.employees.employees)
-  const rows = employees.map((employee, index) => {
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortColumn, setSortColumn] = useState("firstName");
+  const sortedEmployees = employees.sort((a, b) => {
+    const aValue = a[sortColumn];
+    const bValue = b[sortColumn];
+    const sortOrderFactor = sortOrder === "asc" ? 1 : -1;
+
+    if (sortColumn === "startDate" || sortColumn === "birthDay") {
+      return sortOrderFactor * (new Date(aValue) - new Date(bValue));
+    }
+
+    if (aValue < bValue) {
+      return sortOrderFactor * -1;
+    } else if (aValue > bValue) {
+      return sortOrderFactor * 1;
+    } else {
+      return 0;
+    }
+  });
+
+  const rows = sortedEmployees.map((employee, index) => {
     const date = new Date(employee.startDate);
 
     function formatDate(timestamp) {
@@ -33,6 +54,15 @@ function ListEmployee() {
     );
   });
 
+  function handleSort(column) {
+    if (column === sortColumn) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortColumn(column);
+      setSortOrder("asc");
+    }
+  }
+
   return (
     <div className="employee-list_container">
       <h1>Current employees</h1>
@@ -43,31 +73,31 @@ function ListEmployee() {
         <thead>
         <tr>
           <th>
-            <div id="table-first-name">First Name</div>
+            <div onClick={() => handleSort("firstName")} id="table-first-name">First Name</div>
           </th>
           <th>
-            <div id="table-last-name">Last Name</div>
+            <div onClick={() => handleSort("lastName")} id="table-last-name">Last Name</div>
           </th>
           <th>
-            <div id="table-start-date">Start Date</div>
+            <div onClick={() => handleSort("startDate")} id="table-start-date">Start Date</div>
           </th>
           <th>
-            <div id="table-department">Department</div>
+            <div onClick={() => handleSort("department")} id="table-department">Department</div>
           </th>
           <th>
-            <div id="table-date-of-birth">Date of birth</div>
+            <div onClick={() => handleSort("birthDay")} id="table-date-of-birth">Date of birth</div>
           </th>
           <th>
-            <div id="table-street">Street</div>
+            <div onClick={() => handleSort("street")} id="table-street">Street</div>
           </th>
           <th>
-            <div id="table-city">City</div>
+            <div onClick={() => handleSort("city")} id="table-city">City</div>
           </th>
           <th>
-            <div id="table-state">State</div>
+            <div onClick={() => handleSort("state")} id="table-state">State</div>
           </th>
           <th>
-            <div id="table-zip-code">Zip code</div>
+            <div onClick={() => handleSort("zipCode")} id="table-zip-code">Zip code</div>
           </th>
         </tr>
         </thead>
